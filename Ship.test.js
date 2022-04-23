86a2123bf4300897618b4f5761ef99f57d350343
Mohammed <m.alsami@hotmail.com>
2022-04-23 16:28:31 +0100

    ship can be hit multiple times and this is stored

--- Ship.test.js
+++ Ship.test.js
@@ -11,3 +11,9 @@ test("ship is hit in position 2 and ship array is updated", ()=>{
     let threeLengthShip = ship(3);
     expect(threeLengthShip.hit(2)).toEqual([0,'h',2])
 })
+
+test("Ship is hit multiple times",()=>{
+    let fourLengthShip = ship(4);
+    fourLengthShip.hit(1)
+    expect(fourLengthShip.hit(2)).toEqual(['h','h',2,3])
+})
